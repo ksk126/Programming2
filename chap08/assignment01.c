81ee5a67f890daa762b0a9bb74e238a8bd55f8b1
김수경 <peshello014@gmail.com>
2025-09-18 04:26:28 +0900

    .

---
+++ chap08/assignment01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define SIZE 3
+
+void assignment01();
+void print(double* p);
+
+int main()
+{
+	assignment01();
+
+	return 0;
+}
+
+void assignment01()
+{
+	double arr[SIZE] = { 0 };
+	double* p = arr;
+
+	print(p);
+
+	return;
+}
+
+void print(double* p)
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		printf("x[%d]ÀÇ ÁÖ¼Ò: %p\n", i, (void*)&p[i]);
+	}
+}
